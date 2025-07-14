dcc6a80a7507ec26bda5278f05c12ff8146f0539
erfantenhome <40535077+erfantenhome@users.noreply.github.com>
2025-07-14 05:06:48 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ tailscaled &
 # Connect to your network using the auth key and advertise as an exit node
 tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=render-free-vpn --advertise-exit-node &
 
-# Start a simple web server on the port Render provides to keep the service alive
-caddy file-server --listen :${PORT}
+# Start a minimal web server that just responds "OK" on the port Render provides
+caddy respond --listen :${PORT} "OK"
