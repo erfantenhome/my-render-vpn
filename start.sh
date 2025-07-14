41209a75dab7225a38e0397e5068f36330f839ab
erfantenhome <40535077+erfantenhome@users.noreply.github.com>
2025-07-14 05:23:17 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ tailscaled &
 # Connect to your network using the auth key and advertise as an exit node
 tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=render-free-vpn --advertise-exit-node &
 
-# Start a minimal web server that just responds "OK" on the port Render provides
-caddy respond --listen :${PORT} "OK"
+# Start a minimal web server using nc to respond to Render's health checks
+while true; do echo -e 'HTTP/1.1 200 OK\n\nOK' | nc -l -p ${PORT} -w 1; done
